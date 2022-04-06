c728799a97c56c129ca70f282376c79af550c0d4
Turbo Gordz <gordon.draine97@yahoo.com>
2022-04-06 01:19:49 -0400

    framework

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// create the moment function 
+    //just need to make the current time display 
+
+// create the JSON save and parse and stringify fumction for storing local storage items 
+    //make local storage work
+    
+//attach button to save or lock buttons 
+    //make save or lock button 
+    
+
+//optional: make button animate for saving 
